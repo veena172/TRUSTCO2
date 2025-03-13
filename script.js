5fdb403ac9f0d2abcbe2785dca315b539ee393eb
veena172 <veenaanejaa@gmail.com>
2025-03-13 12:53:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const getStartedBtn = document.querySelector(".get-started");
+
+    getStartedBtn.addEventListener("click", function() {
+        alert("Welcome to Trustco!");
+    });
+});
